a68f3544bd29485b50a3b78861f302d9f357f548
gwest <gwest@redhat.com>
2019-06-30 21:01:26 +0800

    First commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+export GIT_COMMIT=$(git rev-parse --short HEAD)
+echo $GIT_COMMIT > wna.version
+go get ./...
+GOOS=windows GOARCH=386 go build -o "wna.exe" -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT"  ./wna
+
+
