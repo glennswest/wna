2015d0935f343be09dbabd0bf2ca22b94d649e7d
gwest <gwest@redhat.com>
2019-07-01 00:52:47 +0800

    Added get and extract for stage2 ignition

--- wna/main.go
+++ wna/main.go
@@ -1,11 +1,13 @@
 package main
 import (
         // "github.com/tidwall/sjson"
-        // "github.com/tidwall/gjson"
-        // "github.com/glennswest/libignition/ignition"
+        "github.com/tidwall/gjson"
+        "github.com/glennswest/libignition/ignition"
         "encoding/base64"
         // "github.com/glennswest/libpowershell/pshell"
         // "encoding/json"
+        "io"
+        "net/http"
         "os"
         "log"
 )
@@ -20,6 +22,35 @@ func main() {
     ignbytes, _ := base64.StdEncoding.DecodeString(ignb64)
     ign := string(ignbytes)
     log.Printf("ign = %v\n",ign)
+    // "ignition":{"config":{"append":[{"source":"
+    append := gjson.Get(ign,"ignition.config.append").String()
+    log.Printf("append = %v\n",append)
+    nodeignsrc := gjson.Get(append,"0.source").String()
+    log.Printf("src = %v\n",nodeignsrc)
+    downloadfile(nodeignsrc,"/k/compute.ignition")
+    ignition.Parse_ignition_file("/k/compute.ignition","/k/ignition")   
+
+}
+
+func downloadfile(filepath string, url string) error {
+
+    // Get the data
+    resp, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    // Create the file
+    out, err := os.Create(filepath)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+
+    // Write the body to file
+    _, err = io.Copy(out, resp.Body)
+    return err
 }
 
 
