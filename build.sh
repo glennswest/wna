a11302ebd83c1d9505741fac2632ef6467a5469e
gwest <gwest@redhat.com>
2019-07-01 23:52:03 +0800

    Add update to node component in build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ echo $GIT_COMMIT > wna.version
 go get ./...
 go build -o wna/wna -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT" ./wna 
 GOOS=windows GOARCH=386 go build -o "wna.exe" -ldflags "-X main.builddate=`date -u +.%Y%m%d.%H%M%S` -X main.gitversion=$GIT_COMMIT"  ./wna
+cp wna.exe ../winoperatordata/files/node_2.0.0/bin/
+cp wna.version ../winoperatordata/files/node_2.0.0/bin/
 
 
